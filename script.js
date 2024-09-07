0081475f9bed03d42c4007f17840c011a6ec30c4
Muhammad Haris Awan <160159398+harisawan27@users.noreply.github.com>
2024-09-07 16:19:17 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.ts
+var toggleButton = document.getElementById('toggle-skills');
+var skillsSection = document.getElementById('skills');
+toggleButton.addEventListener('click', function () {
+    if (skillsSection.style.display === 'none') {
+        skillsSection.style.display = 'block';
+    }
+    else {
+        skillsSection.style.display = 'none';
+    }
+});
